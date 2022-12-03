7c2d2732a4ae5935ce2c5e4af63f03175f46fccc
Carolina Rossello <c.rossello@ufl.edu>
2022-12-03 14:32:24 -0500

    crs commit

--- models/profile.js
+++ models/profile.js
@@ -1,1 +1,14 @@
 const { Schema, model, models } = require('mongoose');
+
+const userInfoSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+      },
+    })
+
+module.exports = models.profile || model('profile', userInfoSchema)
